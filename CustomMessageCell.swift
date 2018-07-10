5bba8e2e893a4b5a3ade2a2dc9b97064174e0cab
Thamer Allahabi <thamer.a.allahabi@gmail.com>
2018-07-10 09:46:53 -0500

    Add files via upload

---
+++ CustomMessageCell.swift
@@ -0,0 +1,26 @@
+//
+//  CustomMessageCell.swift
+//  Fast Chat
+// Thamer Allahabi
+//
+
+import UIKit
+
+class CustomMessageCell: UITableViewCell {
+    
+    
+    @IBOutlet var messageBackground: UIView!
+    @IBOutlet var avatarImageView: UIImageView!
+    @IBOutlet var messageBody: UILabel!
+    @IBOutlet var senderUsername: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        
+        
+        
+    }
+
+
+}
